68c5f9dc737c66f5004e962d37a71372e7142e6c
K33F3 <gabrielle.ting78@gmail.com>
2023-09-30 18:00:39 +0400

    Create Exercise 5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 5.py
@@ -0,0 +1,14 @@
+userRadius = float(input("Please enter radius:\n"))
+print(userRadius)
+
+pi = 3.14
+
+# (A = π r²)
+
+# (outputArea = π userRaidus²)
+
+sqRadius = userRadius * userRadius
+
+outputArea = pi * sqRadius
+
+print(outputArea)
