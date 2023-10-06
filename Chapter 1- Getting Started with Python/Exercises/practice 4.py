29ea323342912188ba8ccdf7c026c791bdb92dc9
K33F3 <gabrielle.ting78@gmail.com>
2023-10-06 20:10:41 +0400

    Create practice 4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/practice 4.py
@@ -0,0 +1,6 @@
+#area for triangle formula A = 1/2 * b * h
+#the height of the triangle is 30cm and the base is 15cm
+base = 15
+height = 30
+a = 1/2 * base * height 
+print("the area of the triangle is", a)
