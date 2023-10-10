56400ba8d5076c2c69d027448afdcce64622b7a1
K33F3 <gabrielle.ting78@gmail.com>
2023-10-10 21:36:43 +0400

    Update Exercise 3.py

--- Chapter 3- Data Structures/Exercises/Exercise 3.py
+++ Chapter 3- Data Structures/Exercises/Exercise 3.py
@@ -1,4 +1,4 @@
 cars = ["Nissan GTR R32" , "Porche GT3 RS" , "Mustang 1969"]
 print("It'd be really nice to own a classy yet packs power type of car like a", cars[2] + ".")
-print("The", cars[0], "is probably one of the most iconis JDM cars out there.")
+print("The", cars[0], "is probably one of the most iconic JDM cars out there.")
 print("The", cars[1], "is a great example of an amazing track car while also having a good look for casual use on certain occasions.")
