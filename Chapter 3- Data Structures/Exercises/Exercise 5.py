7823f24f5f0043cef142d301dd1bc63f4b47351f
K33F3 <gabrielle.ting78@gmail.com>
2023-10-10 21:35:38 +0400

    Create Exercise 5.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 5.py
@@ -0,0 +1,14 @@
+names =["Amy" , "Holt" , "Jake"]
+print(names[0] + " I would like to invite you at an Italian restaurant tonight for a delighful dinner.")
+print(names[1] + " I would like to invite you at an Italian restaurant tonight for a delighful dinner.")
+print(names[2] + " I would like to invite you at an Italian restaurant tonight for a delighful dinner.")
+print("unfortunately It seems like", names[1], "will not be able make it.")
+
+del(names[1])
+names.insert(1 , "Raph")
+print(names)
+
+print(names[0] + " I would like to invite you at an Italian restaurant tonight for a delighful dinner.")
+print(names[1] + " I would like to invite you at an Italian restaurant tonight for a delighful dinner.")
+print(names[2] + " I would like to invite you at an Italian restaurant tonight for a delighful dinner.")
+
