2292d320be9f5fee16391b03c92a1ce30637a3cf
K33F3 <gabrielle.ting78@gmail.com>
2023-10-15 14:00:26 +0400

    Create Exercise 4.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 4.py
@@ -0,0 +1,14 @@
+person_age = 16
+
+if person_age < 2:
+    print("You're a baby")
+elif person_age < 4:
+    print("You're a toddler")
+elif person_age < 13:
+    print("You're a kid")
+elif person_age < 20:
+    print("You're a teenager")
+elif person_age < 65:
+    print("You're an adult")
+elif person_age> 65:
+    print("You're an elder")
