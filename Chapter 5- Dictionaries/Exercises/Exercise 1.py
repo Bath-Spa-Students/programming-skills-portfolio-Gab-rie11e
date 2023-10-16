7dcbebf65fd6430f0fa93e8b08363209bc16edf0
K33F3 <gabrielle.ting78@gmail.com>
2023-10-16 18:31:49 +0400

    Create Exercise 1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 1.py
@@ -0,0 +1,11 @@
+person = {
+    'first_name': 'Jean',
+    'last_name': 'Garcia',
+    'age': 18,
+    'city': 'Valenzuela',
+    }
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
