22a0bc6b7b627385f8b20caba0ac5fb9dfff9568
K33F3 <gabrielle.ting78@gmail.com>
2023-10-16 18:31:51 +0400

    Create Exercise 2.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 2.py
@@ -0,0 +1,22 @@
+glossary = {
+    'comment': 'A note that you can put in a code that will bypass the interpreter.',
+    'list': 'A number of items in brackets.',
+    'string': 'A function that allows user to put words and sentences.',
+    'dictionary': 'A list of key:value pairs.',
+    'variables': 'Variables are letters or words that let us store value in them.',
+}
+
+
+print("\nComment: " + glossary['comment'])
+
+print("\nList: " + glossary['list'])
+
+print("\nString: " + glossary['comment'])
+
+print("\nDictionary: " + glossary['dictionary'])
+
+print("\nVariables: " + glossary['variables'])
+
+
+
+
