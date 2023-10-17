3aa019422f94d279cc8d9d1771be56a3a6b02cf8
K33F3 <gabrielle.ting78@gmail.com>
2023-10-17 09:58:54 +0400

    Update Exercise 2.py

--- Chapter 3- Data Structures/Exercises/Exercise 2.py
+++ Chapter 3- Data Structures/Exercises/Exercise 2.py
@@ -1,4 +1,4 @@
 names = [ "james" , "alex" , "jeanie"]
 print(names[0], "is kinda funny")
-print(names[1], "is a very outgoing person")
-print(names[2], "is the kindest person I've met")
+print(names[1], "is kinda funny")
+print(names[2], "is kinda funny")
