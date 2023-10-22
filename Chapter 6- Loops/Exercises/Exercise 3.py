1e5b20c8e51b6efdc1bfae5e2004df4d54e91ab8
K33F3 <gabrielle.ting78@gmail.com>
2023-10-22 18:30:49 +0400

    Create Exercise 3.py

---
+++ Chapter 6- Loops/Exercises/Exercise 3.py
@@ -0,0 +1,10 @@
+car = (input("What's your favoraite car: "))
+while car != 'toyota':
+    if car == 'nissan':
+       print("Man of culture")
+    elif car == 'porche':
+        print("I like you")
+    elif car == 'mustang':
+        print("classic")
+    else:
+        break
