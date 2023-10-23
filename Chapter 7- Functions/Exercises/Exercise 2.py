15407256283ea03511c30cbdf049813c688df3eb
K33F3 <gabrielle.ting78@gmail.com>
2023-10-23 15:57:23 +0400

    Create Exercise 2.py

---
+++ Chapter 7- Functions/Exercises/Exercise 2.py
@@ -0,0 +1,5 @@
+#I defined favorite book as a function and declared title as a variable
+def favorite_book(title):
+    favorite_book('"If Hearts had Training Wheels"')
+    print(title + " is my favorite book as of the moment")
+
