adccd00d4c5cb707909556a1f068c9ffeee30feb
K33F3 <gabrielle.ting78@gmail.com>
2023-10-23 15:57:33 +0400

    Create Exercise 5.py

---
+++ Chapter 7- Functions/Exercises/Exercise 5.py
@@ -0,0 +1,7 @@
+def describe_city(city, country):
+    description = (city + " is in " + country)
+    print(description)
+
+describe_city('Kyoto' , 'Japan')
+describe_city('Ottawa' , 'Canada')
+describe_city('Amsterdam' , 'Netherlands')
