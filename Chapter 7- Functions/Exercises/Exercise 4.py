7bc4d64f7e4e554d4199e2ccc1e8353534944368
K33F3 <gabrielle.ting78@gmail.com>
2023-10-23 15:57:30 +0400

    Create Exercise 4.py

---
+++ Chapter 7- Functions/Exercises/Exercise 4.py
@@ -0,0 +1,7 @@
+def make_shirt(size='large', message='I love python'):
+    print("\nThe shit's size should be " + size)
+    print("\nAnd it's gonna say " + message)
+
+make_shirt()
+make_shirt(size='medium')
+make_shirt('small' , 'Stay awesome!')
