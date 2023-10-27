810bc12e7c1fa52a4ac29ef0f580821586fc5005
K33F3 <gabrielle.ting78@gmail.com>
2023-10-27 12:10:50 +0400

    Update Exercise 1.py

--- Chapter 7- Functions/Exercises/Exercise 1.py
+++ Chapter 7- Functions/Exercises/Exercise 1.py
@@ -1,6 +1,6 @@
-#I defined the display message as a function
+# Defined the display message as a function
 def display_message():
     learning = "I am currectly learning about functions"
     print(learning)
-
+# Called the function andd printed the message
 display_message()
