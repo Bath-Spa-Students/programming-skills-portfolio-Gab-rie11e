17fe510d2b8ad495cf7d800147b5acea969861d0
K33F3 <gabrielle.ting78@gmail.com>
2023-10-27 13:34:18 +0400

    Update Exercise 3.py

--- Chapter 7- Functions/Exercises/Exercise 3.py
+++ Chapter 7- Functions/Exercises/Exercise 3.py
@@ -1,6 +1,6 @@
 #The process is basically the same as the previous ones
 def make_shirt(size, message):
-    print("The shit's size should be " + size)
+    print("The shirt's size should be " + size)
     print("And it's gonna say " + message)
 
 make_shirt("Medium" , "I love music")
