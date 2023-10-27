4b88a3d758f9edbcc142aa8734c08bbe8292948c
K33F3 <gabrielle.ting78@gmail.com>
2023-10-27 12:08:00 +0400

    Update Exercise 2.py

--- Chapter 6- Loops/Exercises/Exercise 2.py
+++ Chapter 6- Loops/Exercises/Exercise 2.py
@@ -1,11 +1,11 @@
 prompt = "How old are you?"
 
+# I proceed to write a code where the system asks for the user input 
 while True:
     age = input(prompt)
-    if age == 'quit':
-       break
     age = (int(age))
 
+# Input gets processed and compared from all the lines to find the right one to print
     if age < 3:
         print("You're ticket is free!")
     elif age < 12:
