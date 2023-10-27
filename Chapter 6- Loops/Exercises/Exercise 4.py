15e488b82dfde73833e9db7ab365e1fa257c3382
K33F3 <gabrielle.ting78@gmail.com>
2023-10-27 12:09:35 +0400

    Update Exercise 4.py

--- Chapter 6- Loops/Exercises/Exercise 4.py
+++ Chapter 6- Loops/Exercises/Exercise 4.py
@@ -1,15 +1,15 @@
-#here i introduce a list of kinds of sandwiches
+# Introduced a list of kinds of sandwiches
 sandwich_orders = ['cheese' , 'quarter pounder' , 'ham']
 finished_orders = []
 
-#this is where i print the orders that are currently being maded.
+# Print the orders that are currently being maded.
 while sandwich_orders:
     currrent_sandwich = sandwich_orders.pop()
     print("We're preparing your " + currrent_sandwich + " sandwich.")
     finished_orders.append(currrent_sandwich)
 
 
-#now here i print the finished orders.
+# Print the finished orders.
 print('\n')
 for sandwich in finished_orders:
     print("Your " + sandwich + " sandwich is done.")
