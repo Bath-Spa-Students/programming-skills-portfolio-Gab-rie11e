f8aa8d37082c0d8eae86a279931ab50b48992f36
K33F3 <gabrielle.ting78@gmail.com>
2023-10-27 12:08:26 +0400

    Update Exercise 3.py

--- Chapter 6- Loops/Exercises/Exercise 3.py
+++ Chapter 6- Loops/Exercises/Exercise 3.py
@@ -1,3 +1,4 @@
+# Made a loop that never ends when ran, which only stops when the software is closed
 car = (input("What's your favoraite car: "))
 while car != 'toyota':
     if car == 'nissan':
@@ -6,5 +7,3 @@ while car != 'toyota':
         print("I like you")
     elif car == 'mustang':
         print("classic")
-    else:
-        break
