301d563c18dbf64af4b28ee2f223c32e5086a27d
K33F3 <gabrielle.ting78@gmail.com>
2023-10-27 12:11:17 +0400

    Update Exercise 2.py

--- Chapter 7- Functions/Exercises/Exercise 2.py
+++ Chapter 7- Functions/Exercises/Exercise 2.py
@@ -1,5 +1,6 @@
-#I defined favorite book as a function and declared title as a variable
+# Defined favorite book as a function and declared title as a variable
 def favorite_book(title):
-    favorite_book('"If Hearts had Training Wheels"')
     print(title + " is my favorite book as of the moment")
 
+# Stored a value in the title
+favorite_book('"If Hearts had Training Wheels"')
